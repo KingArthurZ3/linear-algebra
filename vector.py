11c26515ef0f1434cd1d8bf009bf3a6215bb258d
Arthur Zhang <arthur@Arthurs-MacBook-Pro-8.local>
2018-12-29 15:54:41 -0800

    added line and vector libraries

---
+++ vector.py
@@ -0,0 +1,126 @@
+import math
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
+class Vector(object):
+
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+    NO_UNIQUE_PARALLEL_COMPONENT_MSG = 'No unique parallel component vector'
+    ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG = 'Only defined in two three dimensional vectors'
+
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
+            self.dimension = len(self.coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def add(self, v):
+        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def minus(self, v):
+        new_coordinates = [x-y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def is_orthogonal_to(self, v, tolerance=1e-10):
+        return abs(self.dot(v)) < tolerance
+
+    def is_parallel_to(self, v):
+        return ( self.is_zero() or v.is_zero()
+                 or self.angle(v) == 0
+                 or self.angle(v) == math.pi)
+
+    def is_zero(self, tolerance=1e-10):
+        return self.magnitude() < tolerance
+
+    def scal_mult(self, c):
+        new_coordinates = [x*Decimal(c) for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    def vec_mult(self, v):
+        return Vector([x*y for (x,y) in zip(self.coordinates, v.coordinates)])
+
+    def magnitude(self):
+        return math.sqrt(sum([pow(x, 2) for x in self.coordinates]))
+
+    def normalized(self):
+        try:
+            return self.scal_mult(Decimal('1.0')/Decimal(self.magnitude()))
+        except ZeroDivisionError:
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+
+    def dot(self, v):
+        return sum([x * y for x,y in zip(self.coordinates, v.coordinates)])
+
+    def angle(self, v, in_degrees=False):
+        try:
+            if not in_degrees:
+                return math.acos(self.normalized().dot(v.normalized()))
+            else:
+                return math.degrees(math.acos(self.normalized().dot(v.normalized())))
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+            else:
+                raise e
+
+    def component_orthogonal_to(self, basis):
+        try:
+            projection = self.component_parallel_to(basis)
+            return self.minus(projection)
+        except Exception as e:
+            if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+                raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            else:
+                raise e
+
+    def component_parallel_to(self, basis):
+        try:
+            u = basis.normalized()
+            weight = self.dot(u)
+            return u.scal_mult(weight)
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+            else:
+                raise e
+
+    def cross(self, v, b=[1,-1,1]):
+        try:
+            coord1 = self.coordinates
+            coord2 = v.coordinates
+            sum1 = b[0] * (coord1[1] * coord2[2] - coord1[2] * coord2[1])
+            sum2 = b[1] * (coord1[0] * coord2[2] - coord1[2] * coord2[0])
+            sum3 = b[2] * (coord1[0] * coord2[1] - coord1[1] * coord2[0])
+            return Vector([sum1, sum2, sum3])
+        except ValueError as e:
+            msg = str(e)
+            if msg == 'need more than 2 values to unpack':
+                self_embedded_in_R3 = Vector(self.coordinates + ('0',))
+                v_embedded_in_R3= Vector(v.coordinates + ('0',))
+                return self_embedded_in_R3.cross(v_embedded_in_R3)
+            elif (msg == 'too many values to unpack' or
+                  msg == 'need more than 1 value to unpack'):
+                raise Exception(self.ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG)
+            else:
+                raise e
+
+    def area_parallelogram(self, v):
+        return self.cross(v).magnitude()
+
+    def area_triangle(self, v):
+        return Decimal(self.area_parallelogram(v)) / Decimal('2')
